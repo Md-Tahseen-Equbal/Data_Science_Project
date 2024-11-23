6612765ada90ebaa907cbdde89f75c6e72d95019
MD Tahseen Equbal <92607061+Md-Tahseen-Equbal@users.noreply.github.com>
2024-11-23 20:56:10 +0530

    Update VisionAssist.py

--- Internship/Innomatic Research Labs/GEN AI/Vision Assist/VisionAssist.py
+++ Internship/Innomatic Research Labs/GEN AI/Vision Assist/VisionAssist.py
@@ -10,7 +10,7 @@ from langchain_google_genai import GoogleGenerativeAI
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Initialize Google Generative AI with API Key
-GEMINI_API_KEY = "AIzaSyCNWcREa6fS9Ph9nzcDM8W2CCGnrcZLxVE"  # Replace with your valid API key
+GEMINI_API_KEY = "Your API Key"  # Replace with your valid API key
 os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
 
 llm = GoogleGenerativeAI(model="gemini-1.5-pro", api_key=GEMINI_API_KEY)
